a068e0eb7a497b5caf3e386fcbb5fcf024f8b96e
Auwalu Muhammad Izziddin <auwalmuhammad5559@gmail.com>
2025-05-08 13:43:22 +0100

    mobile sidebar

--- ai-sass/app/landing/page.tsx
+++ ai-sass/app/landing/page.tsx
@@ -13,6 +13,6 @@ export default function LandingPage(){
       register
     </Link>
     </Button>
-  </div>;
+  </div>
 };
 
--- ai-sass/components/mobile-sidebar.tsx
+++ ai-sass/components/mobile-sidebar.tsx
@@ -2,12 +2,32 @@
 
 import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "./Sidebar";
+import { useEffect, useState } from "react";
 
 const MobileSidebar = () => {
+
+  const [isMounted, setIsMounted] = useState(false); 
+  useEffect(()=>{
+    setIsMounted(true)
+  },[])
+  if (!isMounted) {
+    return null; // Render nothing on the server side
+  }
+
+
   return (
-    <Button variant="ghost" size="icon" className="md:hidden">
+    <Sheet>
+      <SheetTrigger>
+      <Button variant="ghost" size="icon" className="md:hidden">
        <Menu />
-    </Button>
+       </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
   )
 }
 
