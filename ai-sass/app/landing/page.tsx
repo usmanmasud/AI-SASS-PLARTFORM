096ad9bcd791c00033a1caec7a180d6d94cdd179
Auwalu Muhammad Izziddin <auwalmuhammad5559@gmail.com>
2025-05-08 13:18:10 +0100

    sidebar

--- ai-sass/app/landing/page.tsx
+++ ai-sass/app/landing/page.tsx
@@ -1,10 +1,18 @@
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function LandingPage(){
   return <div>
+    <Button>
     <Link href="/sign-in">
       Login
     </Link>
+    </Button>
+    <Button>
+    <Link href="/sign-up">
+      register
+    </Link>
+    </Button>
   </div>;
 };
 
